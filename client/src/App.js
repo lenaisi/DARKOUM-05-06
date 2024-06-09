6e68b39fe8ff886fb2168edcbecb039346d9af81
lenaisi <lenaharchaoui22@gmail.com>
2024-06-09 14:07:02 +0200

    nn

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 // App.js
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-// import { fetchUserData } from './redux/userSlice.js';
+import { fetchUserData } from './redux/userSlice.js';
 import Routes from './routes/Routes.jsx';
 
 const App = () => {
   const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   dispatch(fetchUserData());
-  // }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchUserData());
+  }, [dispatch]);
 
   return (
     <Routes />
